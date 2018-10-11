3930cfb06285348c474f2e8f30fdb5ffcf3781bb
ajithtwot <ajith.2twot@gmail.com>
2018-10-11 12:13:07 +0530

    Initial commit

---
+++ app/src/main/java/com/twot/ajith/audiorecord/MainActivity.java
@@ -0,0 +1,452 @@
+package com.twot.ajith.audiorecord;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.transition.TransitionManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.SimpleMultiPartRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private int RECORD_AUDIO_REQUEST_CODE =123 ;
+    private Toolbar toolbar;
+   // private Chronometer chronometer;
+    ImageView imageViewPlay;
+    private Button btnRecord, btnStop;
+    private SeekBar seekBar;
+    private LinearLayout linearLayoutRecorder, linearLayoutPlay;
+    private MediaRecorder mRecorder;
+    private MediaPlayer mPlayer;
+    private String fileName = null;
+    private int lastProgress = 0;
+    private Handler mHandler = new Handler();
+    private boolean isPlaying = false;
+    private Button send,cancel;
+    public ProgressDialog pDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getPermissionToRecordAudio();
+        }
+        //initializingViews
+        initViews();
+    }
+
+    private void initViews() {
+
+        /** setting up the toolbar  **/
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Voice Recorder");
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.black));
+        setSupportActionBar(toolbar);
+
+        linearLayoutRecorder = (LinearLayout) findViewById(R.id.linearLayoutRecorder);
+       // chronometer = (Chronometer) findViewById(R.id.chronometerTimer);
+//        chronometer.setBase(SystemClock.elapsedRealtime());
+        btnRecord = (Button) findViewById(R.id.btn_start);
+        btnStop = (Button) findViewById(R.id.btn_stop);
+        imageViewPlay = (ImageView) findViewById(R.id.imageViewPlay);
+        linearLayoutPlay = (LinearLayout) findViewById(R.id.linearLayoutPlay);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        send=(Button)findViewById(R.id.btn_send);
+        cancel=(Button)findViewById(R.id.btn_cancel);
+        btnRecord.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+        imageViewPlay.setOnClickListener(this);
+        send.setOnClickListener(this);
+        cancel.setOnClickListener(this);
+        initDialog();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void getPermissionToRecordAudio() {
+        // 1) Use the support library version ContextCompat.checkSelfPermission(...) to avoid
+        // checking the build version since Context.checkSelfPermission(...) is only available
+        // in Marshmallow
+        // 2) Always check for permission (even if permission has already been granted)
+        // since the user can revoke permissions at any time through Settings
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
+
+            // The permission is NOT already granted.
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    RECORD_AUDIO_REQUEST_CODE);
+
+        }
+    }
+
+    // Callback with the request from calling requestPermissions(...)
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == RECORD_AUDIO_REQUEST_CODE) {
+            if (grantResults.length == 3 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[2] == PackageManager.PERMISSION_GRANTED){
+
+                //Toast.makeText(this, "Record Audio permission granted", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(this, "You must give permissions to use this app. App is exiting.", Toast.LENGTH_SHORT).show();
+                finishAffinity();
+            }
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if( view == btnRecord){
+            prepareforRecording();
+            startRecording();
+        }else if( view == btnStop){
+            prepareforStop();
+            stopRecording();
+        }else if( view == imageViewPlay){
+            if( !isPlaying && fileName != null ){
+                isPlaying = true;
+                startPlaying();
+            }else{
+                isPlaying = false;
+                stopPlaying();
+            }
+        }else if(view == send){
+            upload();
+        }
+        else if(view == cancel){
+            dismiss();
+        }
+    }
+
+    private void dismiss() {
+        super.onDestroy();
+    }
+
+    private void upload() {
+//        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+//
+//        StringRequest strReq = new StringRequest(Request.Method.POST, "http://192.168.1.9/guard_patrol/admin_login/app_data/upload_gallery.php", new Response.Listener<String>() {
+//
+//            @Override
+//            public void onResponse(String response) {
+//
+//
+//                // try {
+////                        JSONObject jObj = new JSONObject(response);
+////                        boolean error = jObj.getBoolean("status");
+////                        message = jObj.getString("message");
+////                        if (error) {
+//
+//                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+//
+//                Log.d("response", response);
+////                        } else {
+//
+//                // Error occurred in registration. Get the error
+//                // message
+////                            String errorMsg = jObj.getString("message");
+////                            Toast.makeText(UploadPhotoActivity.this, errorMsg, Toast.LENGTH_LONG).show();
+//                // }
+//            /*       }  catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }*/
+//
+//            }
+//        }, new Response.ErrorListener() {
+//
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(MainActivity.this, "Oops something went wrong...", Toast.LENGTH_LONG).show();
+//
+//            }
+//        }) {
+///*
+//            File sourceFile = new File(filePath);
+//            String image = sourceFile.toString();*/
+//
+//            @Override
+//            protected Map<String, String> getParams() {
+//                // Posting params to register url
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("image",fileName );
+//               /* params.put("imei1", imei1_number);
+//                params.put("imei2", imei2_number);
+//                params.put("timestamp", timestamp);*/
+//                params.put("status", "v");
+//              /*  params.put("latitude", latitude);
+//                params.put("longitude", longitude);*/
+//                return params;
+//            }
+//        };
+//
+//        // Adding request to request queue
+//        //AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+//        RetryPolicy policy = new DefaultRetryPolicy(30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+//        strReq.setRetryPolicy(policy);
+//        queue.add(strReq);
+/*
+        if (postPath == null || postPath.equals("")) {
+            Toast.makeText(this, "please select an image ", Toast.LENGTH_LONG).show();
+            return;
+        } else*/
+showpDialog();
+            SimpleMultiPartRequest smr = new SimpleMultiPartRequest(Request.Method.POST, "http://192.168.1.4/guard_patrol/admin_login/app_data/upload_file.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            hidepDialog();
+                            Log.d("Response", response);
+                            Toast.makeText(getApplicationContext(),"sucess", Toast.LENGTH_LONG).show();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError volleyError) {
+                    hidepDialog();
+                    Toast.makeText(getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            });
+            //smr.addStringParam("param string", " data text");
+            smr.addFile("file", fileName);
+            RequestQueue mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+            mRequestQueue.add(smr);
+
+    }
+
+    private void prepareforRecording() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            TransitionManager.beginDelayedTransition(linearLayoutRecorder);
+        }
+        btnRecord.setVisibility(View.GONE);
+        btnStop.setVisibility(View.VISIBLE);
+        linearLayoutPlay.setVisibility(View.GONE);
+        send.setVisibility(View.GONE);
+        cancel.setVisibility(View.GONE);
+    }
+
+
+    private void startRecording() {
+        //we use the MediaRecorder class to record
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        /**In the lines below, we create a directory named VoiceRecorderSimplifiedCoding/Audios in the phone storage
+         * and the audios are being stored in the Audios folder **/
+        File root = android.os.Environment.getExternalStorageDirectory();
+        File file = new File(root.getAbsolutePath() + "/VoiceRecorderSimplifiedCoding/Audios");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        fileName =  root.getAbsolutePath() + "/VoiceRecorderSimplifiedCoding/Audios/" + String.valueOf(System.currentTimeMillis() + ".mp3");
+        Log.d("filename",fileName);
+        mRecorder.setOutputFile(fileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+            mRecorder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        lastProgress = 0;
+        seekBar.setProgress(0);
+        stopPlaying();
+        //starting the chronometer
+        //chronometer.setBase(SystemClock.elapsedRealtime());
+        //chronometer.start();
+    }
+
+
+    private void stopPlaying() {
+        try{
+            mPlayer.release();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mPlayer = null;
+        //showing the play button
+        imageViewPlay.setImageResource(R.mipmap.ic_play);
+        //chronometer.stop();
+    }
+
+
+    private void prepareforStop() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            TransitionManager.beginDelayedTransition(linearLayoutRecorder);
+        }
+        btnRecord.setVisibility(View.VISIBLE);
+        btnStop.setVisibility(View.GONE);
+        send.setVisibility(View.VISIBLE);
+        cancel.setVisibility(View.VISIBLE);
+        linearLayoutPlay.setVisibility(View.VISIBLE);
+    }
+
+    private void stopRecording() {
+
+        try{
+            mRecorder.stop();
+            mRecorder.release();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mRecorder = null;
+        //starting the chronometer
+        //chronometer.stop();
+        //chronometer.setBase(SystemClock.elapsedRealtime());
+        //showing the play button
+        Toast.makeText(this, "Recording saved successfully.", Toast.LENGTH_SHORT).show();
+    }
+
+    private void startPlaying() {
+        mPlayer = new MediaPlayer();
+        Log.d("instartPlaying",fileName);
+        try {
+            mPlayer.setDataSource(fileName);
+            mPlayer.prepare();
+            mPlayer.start();
+        } catch (IOException e) {
+            Log.e("LOG_TAG", "prepare() failed");
+        }
+        //making the imageview pause button
+        imageViewPlay.setImageResource(R.mipmap.ic_pause);
+
+        seekBar.setProgress(lastProgress);
+        mPlayer.seekTo(lastProgress);
+        seekBar.setMax(mPlayer.getDuration());
+        seekUpdation();
+        //chronometer.start();
+
+        /** once the audio is complete, timer is stopped here**/
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                imageViewPlay.setImageResource(R.mipmap.ic_play);
+                isPlaying = false;
+                //chronometer.stop();
+            }
+        });
+
+        /** moving the track as per the seekBar's position**/
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if( mPlayer!=null && fromUser ){
+                    //here the track's progress is being changed as per the progress bar
+                    mPlayer.seekTo(progress);
+                    //timer is being updated as per the progress of the seekbar
+                    //chronometer.setBase(SystemClock.elapsedRealtime() - mPlayer.getCurrentPosition());
+                    lastProgress = progress;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            seekUpdation();
+        }
+    };
+
+    private void seekUpdation() {
+        if(mPlayer != null){
+            int mCurrentPosition = mPlayer.getCurrentPosition() ;
+            seekBar.setProgress(mCurrentPosition);
+            lastProgress = mCurrentPosition;
+        }
+        mHandler.postDelayed(runnable, 100);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item_list:
+                Intent intent = new Intent(this, RecordingListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+    protected void initDialog() {
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading");
+        pDialog.setCancelable(true);
+    }
+
+
+    protected void showpDialog() {
+
+        if (!pDialog.isShowing()) pDialog.show();
+    }
+
+    protected void hidepDialog() {
+
+        if (pDialog.isShowing()) pDialog.dismiss();
+    }
+}
